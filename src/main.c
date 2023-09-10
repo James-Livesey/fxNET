5798ea36e894b7860f5cc9677b80907b3a9570ef
James-Livesey <jamesliveg@gmail.com>
2023-09-10 19:30:09 +0100

    Change tabs to spaces

--- src/main.c
+++ src/main.c
@@ -4,60 +4,58 @@
 
 #include "serial.h"
 
-char* message = "Hello, world!\n";
-
 int main(void) {
-	serial_init();
+    serial_init();
 
-	dclear(C_WHITE);
-	dtext(0, 0, C_BLACK, "Hello from fxNET!");
-	dupdate();
-	sleep_us(1e6);
+    dclear(C_WHITE);
+    dtext(0, 0, C_BLACK, "Hello from fxNET!");
+    dupdate();
+    sleep_us(1e6);
 
-	if (!serial_open()) {
-		dtext(0, 8, C_BLACK, "Open fail");
-		dupdate();
+    if (!serial_open()) {
+        dtext(0, 8, C_BLACK, "Open fail");
+        dupdate();
 
-		getkey();
+        getkey();
 
-		return 1;
-	}
+        return 1;
+    }
 
-	if (!serial_isOpen()) {
-		dtext(0, 8, C_BLACK, "Not open");
-		dupdate();
+    if (!serial_isOpen()) {
+        dtext(0, 8, C_BLACK, "Not open");
+        dupdate();
 
-		getkey();
+        getkey();
 
-		return 1;
-	}
+        return 1;
+    }
 
-	dtext(0, 8, C_BLACK, "Opened");
-	dupdate();
-	sleep_us(1e6);
+    dtext(0, 8, C_BLACK, "Opened");
+    dupdate();
+    sleep_us(1e6);
 
-	dprint(0, 16, C_BLACK, "Buffer size: %d", serial_txBufferFreeSize());
-	dupdate();
-	sleep_us(1e6);
+    dprint(0, 16, C_BLACK, "Buffer size: %d", serial_txBufferFreeSize());
+    dupdate();
+    sleep_us(1e6);
 
-	char data[4] = {0xDE, 0xAD, 0xBE, 0xEF};
+    char data[4] = {0xDE, 0xAD, 0xBE, 0xEF};
 
-	serial_write(data, sizeof(data));
+    serial_write(data, sizeof(data));
 
-	dtext(0, 24, C_BLACK, "Written");
-	dupdate();
-	sleep_us(1e6);
+    dtext(0, 24, C_BLACK, "Written");
+    dupdate();
+    sleep_us(1e6);
 
-	dclear(C_WHITE);
-	dprint(0, 0, C_BLACK, "Read data: %d", serial_readByte());
-	dupdate();
-	sleep_us(1e6);
+    dclear(C_WHITE);
+    dprint(0, 0, C_BLACK, "Read data: %d", serial_readByte());
+    dupdate();
+    sleep_us(1e6);
 
-	dprint(0, 8, C_BLACK, "Read data: %d", serial_readByte());
-	dupdate();
-	sleep_us(1e6);
+    dprint(0, 8, C_BLACK, "Read data: %d", serial_readByte());
+    dupdate();
+    sleep_us(1e6);
 
-	serial_close();
+    serial_close();
 
-	return 1;
+    return 1;
 }
